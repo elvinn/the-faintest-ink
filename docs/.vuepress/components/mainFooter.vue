1e8845a4950f6385440362a85b9edef020d7710d
Elvin Peng <elvinpeng@tencent.com>
2020-07-06 14:50:48 +0800

    feat: 添加腾讯云静态网站托管的图片展示

---
+++ docs/.vuepress/components/mainFooter.vue
@@ -0,0 +1,63 @@
+<template>
+  <footer>
+    <div class="copyright">
+      Copyright © 2019-{{currentYear}} elvinn
+    </div>
+    <div class="seperator">|</div>
+    <div class="tencent-cloud">
+      Hosted By
+      <a href="https://cloud.tencent.com/product/wh?from=12331">
+        <img src="https://act.weixin.qq.com/static/images/202007/4984fa81348da1ec2e7bdbcdd1749d83_3769x968.png" alt="腾讯云-静态网站托管">
+      </a>
+    </div>  
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'main-footer',
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    }
+  }
+}
+</script>
+
+<style scoped>
+  footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem 0;
+    border-top: 1px solid #eaecef;
+  }
+
+  .seperator {
+    padding: 0 0.6rem;
+  }
+
+  .copyright {
+    text-align: center;
+  }
+
+  .tencent-cloud {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .tencent-cloud img {
+    height: 2rem;
+  }
+
+  @media (max-width: 520px) {
+    footer {
+      flex-direction: column;
+    }
+
+    .seperator {
+      display: none;
+    }
+  }
+</style>
--- docs/README.md
+++ docs/README.md
@@ -3,7 +3,6 @@ home: true
 heroImage: logo.jpg
 heroText: 好记性不如烂笔头
 tagline: elvinn 的个人笔记
-footer: MIT Licensed | Copyright © 2018-present elvinn
 ---
 
 ### 感谢
@@ -11,3 +10,5 @@ footer: MIT Licensed | Copyright © 2018-present elvinn
 1. [VuePress](https://github.com/vuejs/vuepress)
 2. [rawpixel.com / Freepik](https://www.freepik.com)
 3. [腾讯云 - 云开发 CloudBase](https://www.cloudbase.net/)
+
+<main-footer />
