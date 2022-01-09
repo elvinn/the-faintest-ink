d5f6e59837c27ee154485e7ea1af32478a6f39df
elvinpeng <elvinpeng@tencent.com>
2022-01-09 10:15:44 +0800

    fix: 修复 Badge 组件问题

--- docs/.vitepress/components/Badge.vue
+++ docs/.vitepress/components/Badge.vue
@@ -1,6 +1,6 @@
 <template>
   <span :class="['badge', type]" :style="{ verticalAlign }">
-    {text}
+    {{text}}
     <slot></slot>
   </span>
 </template>
@@ -21,22 +21,32 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
-.badge
-  display inline-block
-  font-size 14px
-  height 18px
-  line-height 18px
-  border-radius 3px
-  padding 0 6px
-  color white
-  background-color #42b983
-  &.tip, &.green
-    background-color $badgeTipColor
-  &.error
-    background-color $badgeErrorColor
-  &.warning, &.warn, &.yellow
-    background-color $badgeWarningColor
-  & + &
-    margin-left 5px
+<style scoped>
+.badge {
+  vertical-align: top;
+  display: inline-block;
+  font-size: 14px;
+  height: 18px;
+  line-height: 18px;
+  border-radius: 3px;
+  padding: 0 6px;
+  color: white;
+  background-color: #42b983;
+}
+
+.tip, .green {
+  background-color: #42b983;
+}
+
+.warning, .warn, .yellow {
+  background-color: #e7c000;
+}
+
+.error {
+  background-color: #da5961;
+}
+
+.tip + .tip {
+  margin-left: 5px;
+}
 </style>
