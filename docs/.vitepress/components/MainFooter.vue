82ca9e7318c3c669049f5351c8d24b74d7496508
elvinpeng <elvinpeng@tencent.com>
2022-01-21 16:21:42 +0800

    refactor: vue 组件统一大写开头

---
+++ docs/.vitepress/components/MainFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+    <div class="copyright">
+      Copyright © 2019-{{currentYear}} elvinn
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'main-footer',
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    }
+  }
+}
+</script>
+
+<style scoped>
+  footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem 0;
+    border-top: 1px solid #eaecef;
+  }
+
+  .separator {
+    padding: 0 0.6rem;
+  }
+
+  .copyright {
+    text-align: center;
+  }
+</style>
