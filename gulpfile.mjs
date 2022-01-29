5843380bc556ec006d7bdd21a85b663b58cdcc4f
elvinpeng <elvinpeng@tencent.com>
2022-01-29 11:05:27 +0800

    feat: 引入 sitemap

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -2,14 +2,21 @@ import gulp from 'gulp';
 import imagemin from 'gulp-imagemin';
 import del from 'del';
 
+const DEST = 'docs/.vitepress/dist';
+
 function compressImages() {
 	return gulp.src('docs/**/*.{png,jpg,jpg,gif}')
 		.pipe(imagemin())
-		.pipe(gulp.dest('docs/.vitepress/dist'));
+		.pipe(gulp.dest(DEST));
 };
+
+function copySiteMap() {
+	return gulp.src('./sitemap.xml')
+		.pipe(gulp.dest(DEST));
+}
 
 function clean() {
 	return del('**/.DS_Store');
 }
 
-export default gulp.parallel(compressImages, clean);
+export default gulp.parallel(compressImages, copySiteMap, clean);
