58773880228f907b5781c4e86436fe3d55933685
elvinn <elvin.pjw@gmail.com>
2022-06-19 19:43:00 +0800

    feat: 统一调整页面链接为 .html 后缀

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -29,44 +29,44 @@ export default defineConfig({
         items: [
           {
             text: 'React',
-            link: '/react/problems',
+            link: '/react/problems.html',
             activeMatch: '^/$|^/react/'
           },
           {
             text: 'Vue',
-            link: '/vue/usage',
+            link: '/vue/usage.html',
           },
           {
             text: '小程序',
-            link: '/miniprogram/problems',
+            link: '/miniprogram/problems.html',
           },
           {
             text: 'TypeScript',
-            link: '/typescript/situation',
+            link: '/typescript/situation.html',
           },
           {
             text: 'Node.js',
-            link: '/nodejs/memory',
+            link: '/nodejs/memory.html',
           },
           {
             text: '音视频',
-            link: '/audio-video/file-format',
+            link: '/audio-video/file-forma.html',
           },
           {
             text: '网络',
-            link: '/network/https',
+            link: '/network/https.html',
           },
           {
             text: '工具',
-            link: '/frontend-util/eslint',
+            link: '/frontend-util/eslint.html',
           },
           {
             text: '前端基础',
-            link: '/frontend-basic/trivial'
+            link: '/frontend-basic/trivial.html'
           },
           {
             text: '总结记录',
-            link: '/frontend-other/problems'
+            link: '/frontend-other/problems.html'
           }
         ],
       },
@@ -75,40 +75,40 @@ export default defineConfig({
         items: [
           {
             text: 'C++',
-            link: '/cpp/stl-container',
+            link: '/cpp/stl-container.html',
           },
           {
             text: 'Linux',
-            link: '/linux/file-descriptor',
+            link: '/linux/file-descriptor.html',
           },
           {
             text: 'MySQL',
-            link: '/mysql/usage',
+            link: '/mysql/usage.html',
           },
           {
             text: '常见场景',
-            link: '/backend-common-situation/lock'
+            link: '/backend-common-situation/lock.html'
           }
         ]
       },
       {
         text: 'LeetCode',
-        link: '/leetcode/skill',
+        link: '/leetcode/skill.html',
       },
       {
         text: '阅读',
         items: [
           {
             text: '2022',
-            link: '/reading/2022',
+            link: '/reading/2022.html',
           },
           {
             text: '2021',
-            link: '/reading/2021',
+            link: '/reading/2021.html',
           },
           {
             text: '2020',
-            link: '/reading/2020',
+            link: '/reading/2020.html',
           }
         ],
       },
@@ -117,7 +117,7 @@ export default defineConfig({
         items: [
           {
             text: 'memo',
-            link: '/self-project/memo'
+            link: '/self-project/memo.html'
           }
         ]
       }
@@ -126,127 +126,127 @@ export default defineConfig({
       '/react/': [{
         text: 'React',
         children: [
-          { text: '常见问题', link: '/react/problems' },
-          { text: 'Hook', link: '/react/hooks' },
-          { text: '升级变化', link: '/react/version-update' },
+          { text: '常见问题', link: '/react/problems.html' },
+          { text: 'Hook', link: '/react/hooks.html' },
+          { text: '升级变化', link: '/react/version-update.html' },
         ]
       }],
       '/vue/': [{
         text: 'Vue',
         children: [
-          { text: '基本使用', link: '/vue/usage' },
-          { text: '常见实践总结', link: '/vue/situation' },
+          { text: '基本使用', link: '/vue/usage.html' },
+          { text: '常见实践总结', link: '/vue/situation.html' },
         ]
       }],
       '/miniprogram/': [{
         text: '小程序',
         children: [
-          { text: '基本使用', link: '/miniprogram/problems' },
+          { text: '基本使用', link: '/miniprogram/problems.html' },
         ]
       }],
       '/typescript/': [{
         text: 'TypeScript',
         children: [
-          { text: '常用场景', link: '/typescript/situation' },
-          { text: '常见问题', link: '/typescript/problems' },
-          { text: '基础使用', link: '/typescript/basic' },
-          { text: '编译优化', link: '/typescript/compile-improve' },
+          { text: '常用场景', link: '/typescript/situation.html' },
+          { text: '常见问题', link: '/typescript/problems.html' },
+          { text: '基础使用', link: '/typescript/basic.html' },
+          { text: '编译优化', link: '/typescript/compile-improve.html' },
         ]
       }],
       '/nodejs/': [{
         text: 'Node.js',
         children: [
-          { text: '有意思的 Node.js 内存泄漏问题', link: '/nodejs/memory' },
+          { text: '有意思的 Node.js 内存泄漏问题', link: '/nodejs/memory.html' },
         ]
       }],
       '/audio-video/': [{
         text: '音视频',
         children: [
-          { text: '视频格式', link: '/audio-video/file-format' },
-          { text: '传输协议', link: '/audio-video/transform-protocol' },
+          { text: '视频格式', link: '/audio-video/file-format.html' },
+          { text: '传输协议', link: '/audio-video/transform-protocol.html' },
         ]
       }],
       '/network/': [{
         text: '网络',
         children: [
-          { text: 'HTTPS', link: '/network/https' },
-          { text: 'TCP 协议', link: '/network/tcp' },
-          { text: 'CDN', link: '/network/cdn' },
-          { text: '网络名词释义', link: '/network/glossary' },
+          { text: 'HTTPS', link: '/network/https.html' },
+          { text: 'TCP 协议', link: '/network/tcp.html' },
+          { text: 'CDN', link: '/network/cdn.html' },
+          { text: '网络名词释义', link: '/network/glossary.html' },
         ]
       }],
       '/frontend-util/': [{
         text: '工具',
         children: [
-          { text: 'ESLint', link: '/frontend-util/eslint' },
-          { text: 'npm', link: '/frontend-util/npm' },
-          { text: 'pnpm', link: '/frontend-util/pnpm' },
-          { text: 'Git', link: '/frontend-util/git' },
-          { text: '杂', link: '/frontend-util/mixed' },
+          { text: 'ESLint', link: '/frontend-util/eslint.html' },
+          { text: 'npm', link: '/frontend-util/npm.html' },
+          { text: 'pnpm', link: '/frontend-util/pnpm.html' },
+          { text: 'Git', link: '/frontend-util/git.html' },
+          { text: '杂', link: '/frontend-util/mixed.html' },
         ]
       }],
       '/frontend-basic/': [{
         text: '前端基础',
         children: [
-          { text: '前端基础知识', link: '/frontend-basic/trivial' },
-          { text: '迭代器与生成器', link: '/frontend-basic/iterator' },
+          { text: '前端基础知识', link: '/frontend-basic/trivial.html' },
+          { text: '迭代器与生成器', link: '/frontend-basic/iterator.html' },
         ]
       }],
       '/frontend-other/': [{
         text: '总结记录',
         children: [
-          { text: '问题解决记录', link: '/frontend-other/problems' },
-          { text: '客户端内嵌页面', link: '/frontend-other/native' },
+          { text: '问题解决记录', link: '/frontend-other/problems.html' },
+          { text: '客户端内嵌页面', link: '/frontend-other/native.html' },
         ]
       }],
       '/cpp/': [{
         text: 'C++',
         children: [
-          { text: 'STL 容器', link: '/cpp/stl-container' },
-          { text: '移动语义', link: '/cpp/move-semantic' },
-          { text: '常见问题', link: '/cpp/problems' },
+          { text: 'STL 容器', link: '/cpp/stl-container.html' },
+          { text: '移动语义', link: '/cpp/move-semantic.html' },
+          { text: '常见问题', link: '/cpp/problems.html' },
         ],
       }],
       '/linux/': [{
         text: 'Linux',
         children: [
-          { text: '文件描述符 File Descriptor', link: '/linux/file-descriptor' },
-          { text: '杂', link: '/linux/mixed' },
+          { text: '文件描述符 File Descriptor', link: '/linux/file-descriptor.html' },
+          { text: '杂', link: '/linux/mixed.html' },
         ]
       }],
       '/mysql/': [{
         text: 'MySQL',
         children: [
-          { text: '基本使用', link: '/mysql/usage' },
+          { text: '基本使用', link: '/mysql/usage.html' },
         ]
       }],
       '/backend-common-situation/': [{
         text: '后台常见场景',
         children: [
-          { text: '并发中的锁机制', link: '/backend-common-situation/lock' },
-          { text: '日志滚动切割', link: '/backend-common-situation/log-rotate' },
+          { text: '并发中的锁机制', link: '/backend-common-situation/lock.html' },
+          { text: '日志滚动切割', link: '/backend-common-situation/log-rotate.html' },
         ]
       }],
       '/leetcode/': [{
         text: 'LeetCode',
         children: [
-          { text: '常用技巧', link: '/leetcode/skill' },
-          { text: '好玩的题目', link: '/leetcode/interesting' },
+          { text: '常用技巧', link: '/leetcode/skill.html' },
+          { text: '好玩的题目', link: '/leetcode/interesting.html' },
         ]
       }],
       '/reading/': [{
         text: '阅读',
         children: [
-          { text: '2022 阅读记录', link: '/reading/2022' },
-          { text: '2021 阅读记录', link: '/reading/2021' },
-          { text: '2020 阅读记录', link: '/reading/2020' },
+          { text: '2022 阅读记录', link: '/reading/2022.html' },
+          { text: '2021 阅读记录', link: '/reading/2021.html' },
+          { text: '2020 阅读记录', link: '/reading/2020.html' },
         ]
       }],
       '/self-project/': [{
         text: '个人项目',
         children: [
-          { text: 'memo', link: '/self-project/memo' },
-          { text: 'Oauth 登录', link: '/self-project/oauth' },
+          { text: 'memo', link: '/self-project/memo.html' },
+          { text: 'Oauth 登录', link: '/self-project/oauth.html' },
         ]
       }],
     },
